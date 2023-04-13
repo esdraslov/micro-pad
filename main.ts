6329154763ab1d533ffb38619e3e9be66a6c2e14
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-04-13 17:14:39 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace microPad{
     //% block='on button connected in pin 0 is pressed %f%'
     export function onButtonPressedPZero(f: Function){
         input.onPinPressed(TouchPin.P0, function() {
-            
+            f
         })
     }
 }
